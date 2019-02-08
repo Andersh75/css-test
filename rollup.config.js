e16db37c17396dc9080bf2d457fb3830e2d3f192
Andersh75 <andersh75@gmail.com>
2019-02-08 22:56:45 +0100

    working

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "rollup-plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
 import replace from "rollup-plugin-replace"
+import scss from "rollup-plugin-scss"
 
 export default {
   input: `./src/index.js`,
@@ -15,5 +16,6 @@ export default {
     }),
     resolve(),
     commonjs(),
+    // scss(),
   ],
 }
